d8a51931ee71bad4ef50d44e0d5898f2ae78f748
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-06-30 16:14:02 -0300

    testing OO

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-class Pessoa {
-    nome,
-    sobrenome,
-    idade
+// class Pessoa {
+//     nome,
+//     sobrenome,
+//     idade
+// }
+
+console.log("hello world")
+
+console.log(12344)
+
+let names = {
+    fname: "Moisés",
+    lname: "Nascimento"
 }
+console.log(names)
+console.log(names.hasOwnProperty("fname"))
+
+let pessoa = {
+    nome: 'schwazziplaysson',
+    sobrenome: 'nascimento'
+}
+
+console.log(pessoa)
